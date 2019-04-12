4e9c1d81f0be5f6d59d2142c73a69c2103b29567
Thijs <22070@ma-web.nl>
2019-04-12 14:15:34 +0200

    added most interactable scripts

---
+++ Assets/Scripts/Interactables/WalkInteractable.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkInteractable : Interactable {
+  /// BeamInteract is a empty function becuase we dont want to walk somewhere when the object is hit with a beam 
+  protected override void BeamInteract() { }
+  
+  /// HoldInteract is used to request the PlayerController to move towards the touched point
+  protected override void HoldInteract() {
+    // PlayerController.Instance.RequestMove(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+  }
+
+  /// Interact is used to request the PlayerController to move towards the touched point
+  protected override void Interact() {
+    // PlayerController.Instance.RequestMove(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+  }
+}
