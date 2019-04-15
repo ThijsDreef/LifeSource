3266df21bbf0a7e7d46bf1ce0d28d45f31943d2a
niels <nielsvdmarel@gmail.com>
2019-04-16 00:49:45 +0200

    working point selection. WIP

--- Assets/Scripts/Interactables/WalkInteractable.cs
+++ Assets/Scripts/Interactables/WalkInteractable.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class WalkInteractable : Interactable {
   /// BeamInteract is a empty function becuase we dont want to walk somewhere when the object is hit with a beam 
   protected override void BeamInteract() { }
-  
+  [SerializeField]
+  private GameObject helpObj;
   /// HoldInteract is used to request the PlayerController to move towards the touched point
   protected override void HoldInteract() {
     // PlayerController.Instance.RequestMove(Camera.main.ScreenToWorldPoint(Input.mousePosition));
@@ -13,8 +14,18 @@ public class WalkInteractable : Interactable {
 
   /// Interact is used to request the PlayerController to move towards the touched point
   protected override void Interact() {
+    RaycastHit hit;
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)){
+      PlayerController.Instance.RequestMove(hit.point);
+      Debug.DrawLine(ray.origin, ray.direction * 10000, Color.red);
+      helpObj.transform.position = hit.point;
+    }
+    /*
     Vector3 mousePos = Input.mousePosition;
-    mousePos.z = 10;
+    mousePos.z = Camera.main.nearClipPlane;
+    helpObj.transform.position =  Camera.main.ScreenToWorldPoint(mousePos);
     PlayerController.Instance.RequestMove(Camera.main.ScreenToWorldPoint(mousePos));
+    */
   }
 }
