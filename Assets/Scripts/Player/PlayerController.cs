a6de8d8d8b67062f2384bbb006b688544c192d54
Niels van der Marel <nielsvdmarel@gmail.com>
2019-04-16 16:21:06 +0200

    Events on movement, used by animation.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,54 +7,70 @@ using System;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerController : MonoBehaviour {
-public static PlayerController Instance = null;
-[SerializeField]
-private NavMeshAgent navMeshAgent;
-private bool TargetReached;
-private Coroutine currentRoutine = null;
+  public static PlayerController Instance = null;
+  [SerializeField]
+  private NavMeshAgent navMeshAgent;
+  [SerializeField]
+  private PlayerControllerState currentPlayerState = PlayerControllerState.IDLE;
+  private OnStateAction[] actions = new OnStateAction[(int)PlayerControllerState.LENGTH];
+  private bool TargetReached;
+  private Coroutine currentRoutine = null;
 
-private void Awake(){
-  if(Instance == null){
-    Instance = this;
+  private void Awake() {
+    if (Instance == null) {
+      Instance = this;
+    }
+    else if (Instance != this) {
+      Destroy(gameObject);
+    }
   }
-  else if(Instance != this){
-    Destroy(gameObject);
+
+  private void Start() {
+    navMeshAgent = this.GetComponent<NavMeshAgent>();
   }
-}
 
-private void Start() {
-  navMeshAgent = this.GetComponent<NavMeshAgent>();
-}
+  public void RequestMove(Vector3 TargetDest) {
+    Move(TargetDest, null);
+  }
 
-public void RequestMove(Vector3 TargetDest) {
-  Move(TargetDest, null);
-}
+  public void RequestMove(Vector3 TargetDest, Action CallBack) {
+    Move(TargetDest, CallBack);
+  }
 
-public void RequestMove(Vector3 TargetDest, Action CallBack) {
-  Move(TargetDest, CallBack);
-}
+  public void RequestStartCallback(PlayerControllerState state, Action callback) {
+    actions[(int)state].onStart += callback;
+  }
 
-private void Move(Vector3 TargetDest, Action CallBack) {
-  navMeshAgent.SetDestination(TargetDest);
-  if (currentRoutine != null) StopCoroutine(currentRoutine);
-  currentRoutine = StartCoroutine(Reached(CallBack));
-}
+  private void SetState(PlayerControllerState state) {
+    actions[(int)currentPlayerState].onEnd?.Invoke();
+    currentPlayerState = state;
+    actions[(int)currentPlayerState].onStart?.Invoke();
+  }
 
-private IEnumerator Reached(Action CallBack) {
-  TargetReached = false;
-  do {
-  	if (navMeshAgent.pathPending) {
-      yield return null;
-    }
-    if (navMeshAgent.remainingDistance >= navMeshAgent.stoppingDistance) {
-      yield return null;
-    }
-    if (navMeshAgent.hasPath) {
-      yield return null;
-    } else {
-      TargetReached = true;
-    }
-  } while(!TargetReached);
+  private void Move(Vector3 TargetDest, Action CallBack) {
+    SetState(PlayerControllerState.WALKING);
+    navMeshAgent.SetDestination(TargetDest);
+    if (currentRoutine != null) StopCoroutine(currentRoutine);
+    currentRoutine = StartCoroutine(Reached(CallBack));
+  }
+
+  private IEnumerator Reached(Action CallBack) {
+    TargetReached = false;
+    do {
+      if (navMeshAgent.pathPending) {
+        yield return null;
+      }
+      if (navMeshAgent.remainingDistance >= navMeshAgent.stoppingDistance) {
+        yield return null;
+      }
+      if (navMeshAgent.hasPath) {
+        yield return null;
+      }
+      else {
+        TargetReached = true;
+      }
+    } while (!TargetReached);
     CallBack?.Invoke();
+    SetState(PlayerControllerState.IDLE);
   }
 }
