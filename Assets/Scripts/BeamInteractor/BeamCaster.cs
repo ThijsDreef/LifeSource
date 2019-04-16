9be48a89d4a86639c1d3d6e60144f971cc2b57e7
Rico Evers <ricoevers@live.nl>
2019-04-16 11:34:16 +0200

    Beam interacts with inteactor and optimalisation + AR movement minor fix

--- Assets/Scripts/BeamInteractor/BeamCaster.cs
+++ Assets/Scripts/BeamInteractor/BeamCaster.cs
@@ -6,28 +6,43 @@ using UnityEngine;
 public class BeamCaster : MonoBehaviour
 {
     private List<Vector3> hitPoints = new List<Vector3>();
+    private Vector3 rotationCheck;
+    private GameObject interactable;
     private BeamVisualizer beamVisualizer;
     private const int MAX_DISTANCE = 25;
-    private const int MAX_BOUNCE = 3;
-    // Start is called before the first frame update
+    private const int MAX_BOUNCE = 5;
+
     void Start() {
         beamVisualizer = GetComponent<BeamVisualizer>();
+        rotationCheck = transform.rotation.eulerAngles;
     }
 
     private void FixedUpdate() {
+        if(rotationCheck != transform.rotation.eulerAngles) {
+            UpdateHitPoints();
+        }        
+    }
+
+    /// Called every frame the object is rotated to update the points.
+    private void UpdateHitPoints() {
+        rotationCheck = transform.rotation.eulerAngles;
         hitPoints.Clear();
-        CalculateLine(transform.position, transform.forward);
+        CalculateHitPoints(transform.position, transform.forward);
         beamVisualizer.VisualDraw(hitPoints);
     }
 
-    private void CalculateLine(Vector3 startPosition, Vector3 direction) {
+    /// Cast a ray from the start position to the next object, if that object is able to reflect the beam will reflect to the next until it hits something else or hits nothing.
+    private void CalculateHitPoints(Vector3 startPosition, Vector3 direction) {
         RaycastHit hit;
-        if(hitPoints.Count < MAX_BOUNCE) {
         hitPoints.Add(startPosition);
-        if(Physics.Raycast(startPosition, direction, out hit, MAX_DISTANCE)) {
+        if(Physics.Raycast(startPosition, direction, out hit, MAX_DISTANCE) && hitPoints.Count < MAX_BOUNCE) {
+            if(interactable != hit.collider.gameObject && hit.collider.CompareTag("Reflectable") || hit.collider.CompareTag("Interactable")) {
+                interactable = hit.collider.gameObject;
+                interactable.GetComponent<Interactable>()?.OnBeamHit();
+            }
             if(hit.collider.CompareTag("Reflectable")) {
                 direction = Vector3.Reflect(hit.point - startPosition, hit.normal);
-                CalculateLine(hit.point, direction);
+                CalculateHitPoints(hit.point, direction);
             }
             else {
                 hitPoints.Add(hit.transform.position);
@@ -36,6 +51,5 @@ public class BeamCaster : MonoBehaviour
         else {
             hitPoints.Add(startPosition + direction * MAX_DISTANCE);
         }           
-        }
     }
 }
