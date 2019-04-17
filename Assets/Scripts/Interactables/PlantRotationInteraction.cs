c6a00e7070d16d6f11deb1ab1b1405d26493f081
Rico Evers <ricoevers@live.nl>
2019-04-17 11:06:40 +0200

    Rotate plant to point player looks

---
+++ Assets/Scripts/Interactables/PlantRotationInteraction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantRotationInteraction : RotateInteractor {
+    
+    private Camera mainCamera;
+
+    private void Start() {
+        mainCamera = Camera.main;
+    }
+    
+    /// Points the direction to where the player looks with the camera. 
+    public override IEnumerator Interacting () {
+        while(true) {
+            RaycastHit hit;
+            yield return new WaitForEndOfFrame();
+            if(Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit)) {
+                transform.LookAt(hit.point);
+            }
+        }
+    }
+}
