dcb017d8e1f957b232977bb4f1caedda080af1aa
Thijs <22070@ma-web.nl>
2019-05-08 15:08:49 +0200

    added particle container

---
+++ Assets/Scripts/Vfx/ParticleContainer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleContainer : MonoBehaviour {
+	public static ParticleContainer Instance;
+
+	[SerializeField]
+	private ParticleSystem walkParticle;
+	[SerializeField]
+	private ParticleSystem interactParticle;
+
+
+	private void Start() {
+		if (Instance != null) {
+			Destroy(this);
+			return;
+		}
+		Instance = this;
+	}
+
+	private void EmitParticle(ParticleSystem system, Vector3 position) {
+		system.transform.position = position;
+		system.Emit(10);
+	}
+
+	/// emits the particle for the walk
+	public void EmitWalkParticle(Vector3 position) {
+		EmitParticle(walkParticle, position);
+	}
+
+	/// emits the particle for interacting
+	public void EmitInteractParticle(Vector3 position) {
+		EmitParticle(interactParticle, position);
+	}
+
+}
