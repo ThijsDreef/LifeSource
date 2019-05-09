bc6d351dc7a4111c4d8a354a0f7539d7c57050fe
Niels van der Marel <nielsvdmarel@gmail.com>
2019-05-09 10:16:11 +0200

    FlowerAnim controller done and working ingame.

--- Assets/Scripts/AnimationControllers/PlantAnimationController.cs
+++ Assets/Scripts/AnimationControllers/PlantAnimationController.cs
@@ -5,4 +5,28 @@ using UnityEngine;
 public class PlantAnimationController : MonoBehaviour {
 [SerializeField]
 private Animator animator;
+private PlantRotationInteraction plantRotationInteraction;
+
+private void Start() {
+  plantRotationInteraction = this.GetComponent<PlantRotationInteraction>();
+  plantRotationInteraction.shootStart += ShootStartAnim;
+  plantRotationInteraction.shootStop += ShootEndAnim;
 }
+
+private void ShootStartAnim(){
+animator.SetTrigger("Shoot");
+animator.SetBool("Shooting", true);
+}
+
+private void ShootEndAnim(){
+animator.SetBool("Shooting", false);
+}
+
+private void OnDestroy() {
+  plantRotationInteraction.shootStart -= ShootStartAnim;
+  plantRotationInteraction.shootStop -= ShootEndAnim;
+}
+
+}
+
+
