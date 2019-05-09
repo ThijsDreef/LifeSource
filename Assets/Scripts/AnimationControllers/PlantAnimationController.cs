cbaf90bf3b222d851152e19cf4cd62365489f2a8
Niels van der Marel <nielsvdmarel@gmail.com>
2019-05-09 08:38:25 +0200

    ArtImport

---
+++ Assets/Scripts/AnimationControllers/PlantAnimationController.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantAnimationController : MonoBehaviour {
+[SerializeField]
+private Animator animator;
+}
