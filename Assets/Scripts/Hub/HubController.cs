b335e9f53e515cce1715116d1a56474ac421cc5a
Rico Evers <ricoevers@live.nl>
2019-05-09 10:16:25 +0200

    Hub level setup + hub functionalities

---
+++ Assets/Scripts/Hub/HubController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubController : MonoBehaviour {
+    public List<GameObject> levelHub = new List<GameObject>();
+
+    private void Awake() { 
+        for(int i = 0; i < LevelHandler.Instance.levels.Count; i++) {
+            GameObject copy = Instantiate(levelHub[i], new Vector3(Mathf.Cos(Mathf.Deg2Rad * 360 * ((float)i / LevelHandler.Instance.levels.Count)) * 40, 15, Mathf.Sin(Mathf.Deg2Rad * 360 * ((float)i / LevelHandler.Instance.levels.Count)) * 40), Quaternion.identity);
+            copy.transform.parent = this.transform;     
+        }
+    }  
+}
---
+++ Assets/Scripts/Hub/LevelHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelHandler : MonoBehaviour {
+
+    public static LevelHandler Instance;
+    public List<GameObject> levels = new List<GameObject>();
+    public List<int> unlockedLevels = new List<int>();
+
+    private void Awake() {
+
+        if(Instance == null) {
+            Instance = this;
+        }
+        else {
+            Destroy(this);
+        }
+
+        for(int i = 0; i < levels.Count; i++) {
+            unlockedLevels.Add(PlayerPrefs.GetInt("UnlockState" + i));
+        }
+    }
+
+    public void ChangeLevel(int levelIndex) {
+        if(unlockedLevels.Contains(levelIndex)) {
+            for(int i = 0; i < levels.Count; i++) {
+                levels[i].SetActive(false);
+            }
+            levels[levelIndex].SetActive(true);
+        }
+    }
+
+    public void UnlockLevel(int unlockIndex) {
+        if(!unlockedLevels.Contains(unlockIndex)) {
+            unlockedLevels[unlockIndex] = unlockIndex;
+            PlayerPrefs.SetInt("UnlockState" + unlockIndex, unlockIndex);
+        }
+    }
+}
