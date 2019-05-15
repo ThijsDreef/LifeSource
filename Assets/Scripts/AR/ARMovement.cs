d4f045432cef732cba1beaee48ade408f952ef23
Rico Evers <ricoevers@live.nl>
2019-05-15 12:52:57 +0200

    Loading of levels now done with async additive laoding

--- Assets/Scripts/AR/ARMovement.cs
+++ Assets/Scripts/AR/ARMovement.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class ARMovement : MonoBehaviour
 {
-    public Transform imageTarget;
+    private Transform imageTarget;
     public float smoothness;
     public float rotationStrength;
     private Vector3 velocity = Vector3.zero;
 
+    private void Awake() {
+        imageTarget = GameObject.FindGameObjectWithTag("Target").transform;
+    }
+
     /// Lest the object move smooth between positions, helps against jitters.
-    void LateUpdate() {
+    private void LateUpdate() {
         transform.position = Vector3.SmoothDamp(transform.position, imageTarget.position, ref velocity, smoothness);
         transform.rotation = Quaternion.Slerp(transform.rotation, imageTarget.rotation, rotationStrength);
     }
--- Assets/Scripts/Hub/LevelHandler.cs
+++ Assets/Scripts/Hub/LevelHandler.cs
@@ -1,42 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelHandler : MonoBehaviour {
 
     public static LevelHandler Instance;
-    public List<GameObject> levels = new List<GameObject>();
-    public List<int> unlockedLevels = new List<int>();
+    public int levelCount;
+    public GameObject spawnPoint;
+    private List<int> unlockedLevels = new List<int>();
     private int currentLevelIndex;
+    private AsyncOperation loadScene;
 
-    private void Awake() {
+    private void Start() {
 
         if(Instance == null) {
             Instance = this;
         }
         else {
             Destroy(this);
+            return;
         }
 
-        for(int i = 0; i < levels.Count; i++) {
+        for(int i = 0; i < levelCount; i++) {
             unlockedLevels.Add(PlayerPrefs.GetInt("UnlockState" + i));
         }
+        unlockedLevels[1] = 1;
+        unlockedLevels[2] = 2;
+        ChangeLevel(1);
     }
 
     /// Disable all levels and then enables the given level if it is unlockd.
     public void ChangeLevel(int levelIndex) {
-        if(unlockedLevels.Contains(levelIndex)) {
-            currentLevelIndex = levelIndex;
+        if(unlockedLevels.Contains(levelIndex) && currentLevelIndex != levelIndex) {
             OverlayController.Instance.onEndOverlay.AddListener(SetupLevel);
             OverlayController.Instance.StartOverlay();
+            loadScene = SceneManager.LoadSceneAsync(levelIndex, LoadSceneMode.Additive);
+            loadScene.allowSceneActivation = false;
+            if(currentLevelIndex != 0) {
+                SceneManager.UnloadSceneAsync(currentLevelIndex, UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
+            }
+            currentLevelIndex = levelIndex;
         }
     }
 
+    /// Activates the scene and removes the overlay.
     private void SetupLevel() {
-        for(int i = 0; i < levels.Count; i++) {
-            levels[i].SetActive(false);
-        }
-        levels[currentLevelIndex].SetActive(true);
+        spawnPoint.SetActive(false);
+        loadScene.allowSceneActivation = true;
         OverlayController.Instance.onEndOverlay.RemoveListener(SetupLevel);
     }
 
