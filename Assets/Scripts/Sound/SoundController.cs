a058710cfeb1174fa90ed88047594d0078e398c8
Rico Evers <ricoevers@live.nl>
2019-05-29 10:00:35 +0200

    Sounds added and controller created

---
+++ Assets/Scripts/Sound/SoundController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioMixerGroup audioMixerGroup;
+    public static SoundController Instance;
+
+    public Sound[] sounds;
+    private void Awake() {
+        if (Instance != null) {
+            Destroy(gameObject);
+        } else {
+            Instance = this;
+        }
+    }
+
+    private void Start() {
+        AudioSourcesCreator();
+    }
+
+    private void AudioSourcesCreator() {
+        for (int i = 0; i < sounds.Length; i++) {
+            GameObject _go = new GameObject("Sound_" + i + "_" + sounds[i].soundName);
+            _go.transform.SetParent(this.transform);
+            AudioSource audioSource = _go.AddComponent<AudioSource>();
+            audioSource.outputAudioMixerGroup = audioMixerGroup;
+            sounds[i].SetSource(audioSource);
+        }
+    }
+
+    public void PlaySound(string _soundName) {
+        for (int i = 0; i < sounds.Length; i++) {
+            if (sounds[i].soundName == _soundName) {
+                StartCoroutine(Delay(i, sounds[i].delay));
+                return;
+            }
+        }
+        //no sounds found
+        Debug.LogWarning("AudioManager: Sound not found in list. " + _soundName);
+    }
+
+    private IEnumerator Delay(int index, float delayTime) {
+        yield return new WaitForSeconds(delayTime);
+        sounds[index].Play();
+    }
+}
