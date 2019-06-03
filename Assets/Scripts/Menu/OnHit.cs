24c60f44a3cebefcc9bb30d44b88c23a59598394
Rico Evers <ricoevers@live.nl>
2019-06-03 11:06:57 +0200

    Added sound functions

---
+++ Assets/Scripts/Menu/OnHit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHit : MonoBehaviour {
+
+    public ParticleSystem touchParticle;
+
+    private void Update() {
+        //Camera.main.WorldToScreenPoint
+        Vector3 hit;
+        #if (UNITY_IPHONE || UNITY_ANDROID) && !UNITY_EDITOR 
+            if(Input.touches.Length > 0) {
+                hit = Camera.main.ViewportToWorldPoint(Input.touches[0].position);
+                hit.z = 90;
+                touchParticle.gameObject.transform.position = hit;
+                touchParticle.Play();
+            }
+            
+        #else
+            if(Input.GetMouseButtonDown(0)) {
+                print("Test");
+                hit = Camera.main.ViewportToWorldPoint(Input.mousePosition);
+                hit.z = 90;
+                touchParticle.gameObject.transform.position = hit;
+                touchParticle.Play();
+            }
+        #endif
+
+        
+    }
+}
--- Assets/Scripts/Sound/RemoteSound.cs
+++ Assets/Scripts/Sound/RemoteSound.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RemoteSound : MonoBehaviour {
-    
+
     public void Play(string soundName) {
         SoundController.Instance.PlaySound(soundName);
     }
