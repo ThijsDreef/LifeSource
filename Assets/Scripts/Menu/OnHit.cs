1b281ba8a047046f5473082c077404de4be81e27
Rico Evers <ricoevers@live.nl>
2019-06-04 10:36:26 +0200

    Added new sounds and fixed menu particle

--- Assets/Scripts/Menu/OnHit.cs
+++ Assets/Scripts/Menu/OnHit.cs
@@ -3,26 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OnHit : MonoBehaviour {
-
-    public ParticleSystem touchParticle;
+    [SerializeField]
+    private ParticleSystem touchParticle;
+    [SerializeField]
+    private GameObject touchParticleObject;
+    [SerializeField]
+    private RectTransform canvasRectTransform;
 
     private void Update() {
         //Camera.main.WorldToScreenPoint
         Vector3 hit;
         #if (UNITY_IPHONE || UNITY_ANDROID) && !UNITY_EDITOR 
             if(Input.touches.Length > 0) {
-                hit = Camera.main.ViewportToWorldPoint(Input.touches[0].position);
-                hit.z = 90;
-                touchParticle.gameObject.transform.position = hit;
+                hit = new Vector3(((Input.mousePosition.x / Camera.main.pixelRect.width) - 0.5f) * canvasRectTransform.rect.width, ((Input.mousePosition.y / Camera.main.pixelRect.height) - 0.5f) * canvasRectTransform.rect.height, 0);
+                touchParticleObject.transform.localPosition = hit;
                 touchParticle.Play();
             }
             
         #else
             if(Input.GetMouseButtonDown(0)) {
-                print("Test");
-                hit = Camera.main.ViewportToWorldPoint(Input.mousePosition);
-                hit.z = 90;
-                touchParticle.gameObject.transform.position = hit;
+                hit = new Vector3(((Input.mousePosition.x / Camera.main.pixelRect.width) - 0.5f) * canvasRectTransform.rect.width, ((Input.mousePosition.y / Camera.main.pixelRect.height) - 0.5f) * canvasRectTransform.rect.height, 0);
+                touchParticleObject.transform.localPosition = hit;
                 touchParticle.Play();
             }
         #endif
