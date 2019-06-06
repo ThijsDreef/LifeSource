0060ba3c0408a7205d3317d3170b693862177cce
Niels van der Marel <nielsvdmarel@gmail.com>
2019-06-06 10:22:22 +0200

    Revert "Revert "Merge branch 'develop' into feature/level6Dev""
    This reverts commit e33463a5cd5362b7828672761f07d965ebb43dd9.

---
+++ Assets/Scripts/Sound/SoundController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioMixerGroup SFXMixer;
+    public static SoundController Instance;
+    public Sound[] SFX;
+    private void Awake() {
+        if (Instance != null) {
+            Destroy(gameObject);
+        } else {
+            Instance = this;
+        }
+    }
+
+    private void Start() {
+        AudioSourcesCreator();
+    }
+
+    private void AudioSourcesCreator() {
+        for (int i = 0; i < SFX.Length; i++) {
+            GameObject _go = new GameObject("Sound_" + i + "_" + SFX[i].soundName);
+            _go.transform.SetParent(this.transform);
+            AudioSource audioSource = _go.AddComponent<AudioSource>();
+            audioSource.outputAudioMixerGroup = SFXMixer;
+            SFX[i].SetSource(audioSource);
+        }
+    }
+
+    public void PlaySound(string _soundName) {
+        for (int i = 0; i < SFX.Length; i++) {
+            if (SFX[i].soundName == _soundName) {
+                StartCoroutine(Delay(i, SFX[i].delay));
+                return;
+            }
+        }
+        //no sounds found
+        Debug.LogWarning("AudioManager: Sound not found in list. " + _soundName);
+    }
+
+    private IEnumerator Delay(int index, float delayTime) {
+        yield return new WaitForSeconds(delayTime);
+        SFX[index].Play();
+    }
+
+    public void ToggleAudio(string type) {
+        float volume;
+        SFXMixer.audioMixer.GetFloat(type, out volume);
+        if(volume == 0) {
+            SFXMixer.audioMixer.SetFloat(type, -80);
+        }
+        else {
+            SFXMixer.audioMixer.SetFloat(type, 0);
+        }
+        
+    }
+}
